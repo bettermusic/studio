16c714900036d8182592b7ec99a9afd56d9494e6
isaiah dahl <isaiahdahl@me.com>
2022-05-30 15:32:19 -0700

    messing with turborepo

--- packages/praisecharts-core/src/Button.tsx
+++ packages/praisecharts-core/src/Button.tsx
@@ -5,6 +5,15 @@ export interface ButtonProps {
 }
 
 export function Button(props: ButtonProps) {
+//   const chordSheet = `
+//        Am         C/G        F          C
+// Let it be, let it be, let it be, let it be
+// C                G              F  C/E Dm C
+// Whisper words of wisdom, let it be`.substring(1);
+
+// const parser = new ChordSheetJS.ChordSheetParser();
+// const song = parser.parse(chordSheet);
+
   return <button>{props.children} Today</button>;
 }
 
