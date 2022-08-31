aaa05137df6ef301cc00dcfdd89565f2cd770642
Brandon Keepers <brandon@opensoul.org>
2022-08-31 14:12:21 -0400

    Update highlight theme

--- packages/codemirror-lang-chordpro/src/index.ts
+++ packages/codemirror-lang-chordpro/src/index.ts
@@ -2,19 +2,15 @@ import {parser} from "./syntax.grammar"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
 import {styleTags, Tag, tags as t} from "@lezer/highlight"
 
-export const customTags = {
-  chord: Tag.define(), // define custom tag, that can be picked up by the style configuration
-}
-
 export const ChordProLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       styleTags({
-        Chord: customTags.chord,
-        MetaData: t.meta,
+        Chord: t.keyword,
+        DirectiveName: t.meta,
+        DirectiveValue: t.attributeValue,
         Comment: t.lineComment,
-        Bracket: t.squareBracket,
-        String: t.string,
+        Lyric: t.content,
       })
     ]
   }),
--- packages/studio/src/utils/codemirror.utils.ts
+++ packages/studio/src/utils/codemirror.utils.ts
@@ -1,11 +1,9 @@
 import { Compartment, Text} from "@codemirror/state"
 import {HighlightStyle} from "@codemirror/language"
-import {customTags} from "codemirror-lang-chordpro"
 import {tags} from "@lezer/highlight"
 import { hoverTooltip} from "@codemirror/view"
 import {EditorView, Panel} from "@codemirror/view"
 import {showPanel} from "@codemirror/view"
-
 
 export const myTheme = EditorView.theme({
     "&": {
@@ -31,13 +29,15 @@ export const myTheme = EditorView.theme({
     }
 }, {dark: true})
 
-export const language = new Compartment 
+export const language = new Compartment
 
 export const myHighlightStyle = HighlightStyle.define([
-  {tag: customTags.chord, color: "#0481aa"},
+  {tag: tags.keyword, color: "#0481aa"},
   {tag: tags.bracket, color: "#fc6"},
-  {tag: tags.meta, color: "#fa7c6c"},
-  {tag: tags.comment, color: "#5d6063"}
+  {tag: tags.meta, color: "#8a8c8d"},
+  {tag: tags.attributeValue, color: "#fa7c6c"},
+  {tag: tags.lineComment, color: "#5d6063"},
+  {tag: tags.content, color: '#fff'}
 ])
 
 export const wordHover = hoverTooltip((view, pos, side) => {
