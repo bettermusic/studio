39d17abd0603224609c63718879bd13ad1cfad80
isaiah dahl <isaiahdahl@me.com>
2022-11-18 08:33:21 -0800

    feat: adjust styling of chart

--- packages/studio/src/utils/example-chordpro.ts
+++ packages/studio/src/utils/example-chordpro.ts
@@ -3,9 +3,86 @@ export default `
 {artist: Maverick City Music}
 {key: F}
 
-My [Dm7]heart has always [C/E]longed for something[F] more
-I [Dm7]searched the stars to [Bb]knock on Heaven's [F2]door
-Cre - [Dm7]ation groans for [C2/E]God to be re [F2]- vealed
-[Dm7]Every wound we [Bb]carry will be[F2] healed
-My eyes on the [G7/B]Son, Lord Your will be [Bbm6]done
+{c:Intro (5x)}
+[Eb(no3)][/][/][/][|][/][/][/][/][|]
+
+{c:Verse 1}
+[Eb]Saturday was silent, [Ebsus]surely it was [Eb]through
+[Cm] But since when has im - [Bb]possible [Ab2]ever [Eb/G]stopped [Eb]You[/][/][/][/]
+[Eb]Friday’s disappointment is Sun - [Ebsus]day’s empty [Eb]tomb
+[Cm] Since when has im - [Bb]possible [Ab2]ever [Eb/G]stopped [Eb]You
+[Ab2][-][Eb/G][Eb][/]
+
+{c:Chorus 1}
+[Eb5]This is the sound of dry bones rattling[/Bb][-][/Ab][Eb5]
+[Eb5]This is the praise make a dead man walk again[/Bb][-][/Ab][Eb5]
+Open the [Cm]grave, I'm coming [Bb]out
+I'm gonna [Ab]live, gonna live a - [Eb]gain
+[Eb5]This is the sound of dry bones rattling[Ab][-][Eb/G][Eb5]
+[Ab][-][Eb/G][Eb5][/][|][Ab][-][Eb/G][Eb5][/]
+
+{c:Verse 2}
+[Eb]Pentecostal fire [Ebsus]stirring something [Eb]new
+[Cm] You're not gonna [Bb]run out of miracles [Ab2]any - [Eb/G]time [Eb]soon
+[Eb]Resurrection power [Ab]runs in my veins [Eb/Bb]too
+[Cm] I believe there's a - [Bb]nother miracle [Ab2]here [Eb/G]in [Eb]this [Eb]room
+
+{c:Chorus 2}
+[Eb5]This is the sound of dry bones rattling[Bb][-][Ab][Eb]
+[Eb]This is the praise make a dead man walk again[Bb][-][Ab][Eb]
+Open the [Cm]grave, I'm coming [Bb]out
+I'm gonna [Ab]live, gonna live a - [Eb]gain
+[Eb]This is the sound of dry bones rattling[Ab][-][Eb/G][Eb]
+[Ab][-][Eb/G][Eb]
+
+{c:Instrumental 1}
+[(Eb)][/][/][/][|][/][/][/][/][|][/][/][/][/][|]
+[Eb5][/][/][/][|][/][/][/][/][|][/][/][/]
+
+{c:Bridge 1}
+My God is [Eb5]able to save and deliver and heal
+And restore anything that He wants to
+Just ask the [Eb5]man who was thrown on the bones of Elisha
+If there’s anything that He can’t do
+
+{c:Bridge 2}
+Just ask the [Ab]stone [Eb/G]that was [Eb]rolled at the [Ab]tomb [Eb/G]in the [Eb]garden
+What [Ab]hap - [Eb/G]pens when [Eb]God says to [Ab]move[-][Eb/G][Eb]
+I feel Him [Ab]mov - [Eb/G]ing it [Eb]now, I feel Him [Ab]do - [Eb/G]ing it [Eb]now
+I feel Him [Ab]do - [Eb/G]ing it [Eb]now, do it now,[Cm] [Bb]do it [Ab]now
+
+{c:Chorus 2}
+[Eb5]This is the sound of dry bones rattling[Bb][-][Ab][Eb]
+[Eb]This is the praise make a dead man walk again[Bb][-][Ab][Eb]
+Open the [Cm]grave, I'm coming [Bb]out, I'm gonna [Ab]live, gonna live a - [Eb]gain
+Open the [Cm]grave, I'm coming [Bb]out, I'm gonna [Ab]live, gonna live a - [Eb]gain
+Open the [Cm]grave, I'm coming [Bb]out, I'm gonna [Ab]live, gonna live a - [Eb]gain
+[Eb]This is the sound of dry bones rattling[Ab][-][Eb/G][Eb][/]
+
+{c:Instrumental 2}
+[Eb][/][/][/][|][/][/][/][/][|][/][/][/][/][|]
+
+{c:Bridge 3 (2x)}
+[Eb5]I hear the sound (I hear the sound)
+I hear the sound (I hear the sound)
+
+{c:Bridge 4}
+[Eb] [Db/Eb] [Cm/Eb] And the bones be - [Cb/Eb]gin to rattle!
+[Eb] (Rattle![Db/Eb] [Cm/Eb]Rattle!)[Cb/Eb]
+[Eb](Rattle! [Db/Eb]Rattle!) [Cm/Eb]God said live, [Cb/Eb](God said live)
+[Eb] [Db/Eb] Hear the [Cm/Eb]word of the Lord[Cb/Eb]
+
+{c:Bridge 5}
+And this is what he [Eb]said
+Live, live, dry bones hear the word of the Lord
+Live (live), live (live), dry bones hear the word of the Lord
+Live (live), live (live), dry bones hear the word of the Lord
+Live (live), live (live), dry bones hear the word of the Lord
+[Eb]Live (live), [Db/Eb]live (live),[Cm/Eb] dry bones hear the [Cb/Eb]word of the Lord
+[Eb]Live (live), [Db/Eb]live (live), [Cm/Eb]live, live, [Cb/Eb]live, live
+
+{c:Chorus 2}
+
+{c:End}
+[Ab][-][Eb/G][Eb][/][|][Ab][-][Eb/G][Eb][/][|]
 `.substring(1);
