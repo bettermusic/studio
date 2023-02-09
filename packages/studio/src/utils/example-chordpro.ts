ce049b4754f7d2f9ad775bd320f339e1c035a520
isaiah dahl <isaiahdahl@me.com>
2023-02-09 10:10:01 -0800

    feat: pull in master for chordsheetjs, include patch for now

--- packages/studio/src/utils/example-chordpro.ts
+++ packages/studio/src/utils/example-chordpro.ts
@@ -7,10 +7,4 @@ Eb5 / / / | / / / /
 Verse 1
 Eb                   Ebsus         Eb
 Saturday was silent, surely it was through
-Cm                         Bb       Ab2  Eb/G    Eb / / / /
-   But since when has im - possible ever stopped You
-Eb                               Ebsus       Eb
-Friday’s disappointment is Sun - day’s empty tomb
-Cm                     Bb       Ab2  Eb/G    Eb
-   Since when has im - possible ever stopped You
 `.substring(1);
